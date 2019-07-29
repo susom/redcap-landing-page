f52c152ae502151e8ab2cdb991bb647927636ed7
Irvin Szeto <irvins@stanford.edu>
2019-07-29 14:37:15 -0700

    try caching the mp4 video for better page performance

--- LandingPage.php
+++ LandingPage.php
@@ -2,12 +2,14 @@
 namespace Stanford\LandingPage;
 
 include_once "emLoggerTrait.php";
+include_once "emNotifsTrait.php";
 
 // NOTE That for this to work in shibboleth, you must add a define("NOAUTH",true) to the main index.php before redcap connect.
 
 class LandingPage extends \ExternalModules\AbstractExternalModule {
 
     use emLoggerTrait;
+    use emNotifsTrait;
 
 	function redcap_every_page_before_render($project_id = null){
         global $lang, $redcap_version;
@@ -24,13 +26,11 @@ class LandingPage extends \ExternalModules\AbstractExternalModule {
             'PAGE FULL: ' . PAGE_FULL,
             'URI: ' . $_SERVER['REQUEST_URI']);//, $_SERVER['asdf']);
 
-
-
         // Take over REDCap home page
         if ( (PAGE == "index.php" || PAGE == "/") && empty($base) ){
 
             // Lets take over this page and prevent other code from executing
-             $this->emDebug("Run Landing Page - " . PAGE);
+            $this->emDebug("Run Landing Page - " . PAGE);
 
             $authenticatedHomeUrl = APP_PATH_WEBROOT_FULL . "redcap_v" . $redcap_version . "/home/index.php";
 
@@ -39,10 +39,11 @@ class LandingPage extends \ExternalModules\AbstractExternalModule {
             if (!empty($_GET['action']) || !empty($_GET['route'])) {
                 $newUrl = $authenticatedHomeUrl . "?" . $_SERVER['QUERY_STRING'];
                 $this->emDebug("NEW URL: " . $newUrl);
-                header("Location: " . $newUrl);
+                // header("Location: " . $newUrl);
             }
 
 
+            
             // MUST BE LOGGED IN
             // action=myprojects, create,
             // route=SenditController:upload,
@@ -52,37 +53,12 @@ class LandingPage extends \ExternalModules\AbstractExternalModule {
             // nav-link navbar-user-messaging
 
             // ONLY SHOW USERNAME AND ICON IF LOGGED IN OTHERWISE SHOW LOGIN BUTTON
-
-
+            
             include $this->getModulePath() . "/landing_page.php";
             $this->exitAfterHook();
-
-            // $this->exitAfterHook();
         } else {
             $this->emDebug("Page is " . PAGE. " - skipping");
         }
-    }
-
-    public function getSystemSubSettings($key) {
-        $keys   = [];
-        $config = $this->getSettingConfig($key);
-        if ($config['type'] !== "sub_settings") return false;
-
-        // Get the keys that are part of this subsetting
-        foreach ($config['sub_settings'] as $subSetting) {
-            $keys[] = $subSetting['key'];
-        }
-
-        // Loop through the keys to pull values from $settings
-        $subSettings = [];
-        foreach ($keys as $key) {
-            $values = $settings[$key];
-            foreach ($values as $i => $value) {
-                $subSettings[$i][$key] = $value;
-            }
-        }
-
-        return $subSettings;
     }
 
 }
